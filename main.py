fb2cfee9e5ff228afce4ff2645bd913d8d52b1aa
0x0u <m0zurillex@gmail.com>
2021-01-30 02:24:23 +0900

    :+1: 関数アノテーションの追加、docstringの追加、全体的なコードの改修などをした

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from typing import List, Dict, Optional
 import firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import datetime, timedelta, timezone
@@ -7,8 +8,12 @@ from linebot.models import TextSendMessage
 
 import config
 
+
 # デバッグ
 DEBUG = config.DEBUG
+
+# 日本標準時
+jst = timezone(timedelta(hours=+9), 'JST')
 
 # line-bot-sdk初期化
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
@@ -23,104 +28,129 @@ else:
     firebase_admin.initialize_app()
 db = firestore.client()
 
-rooms = {}
-reservations = []
 
+def get_rooms_data(now: datetime) -> Optional[Dict]:
+    """
+    現在の学習室の空席情報の取得
 
-def init_var():
-    print('### init_var ###')
-    global rooms, reservations
-    rooms = {}
-    reservations = []
+    Args:
+        now(datetime): 現在時刻
 
-
-def get_time():
-    jst = timezone(timedelta(hours=+9), 'JST')
-    return datetime.now(jst)
-
-
-def get_rooms():
+    Returns:
+        dict or None: 空席情報を取得した場合はDictを返す
+    """
     print('### get_rooms ###')
-    global rooms
-    now = get_time()
+    # Debug用変数
+    # date = '20210129'
+    # time = '1705'
     date = now.strftime('%Y%m%d')
     time = now.strftime('%H%M')
     rooms_ref = db.collection('rooms').document(date).get()
     if rooms_ref.exists:
-        rooms = rooms_ref.to_dict().get(time)
-        if not rooms:
+        rooms_data = rooms_ref.to_dict().get(time)
+        if not rooms_data:
             before = (now - timedelta(minutes=1)).strftime('%H%M')
-            rooms = rooms_ref.to_dict().get(before)
+            rooms_data = rooms_ref.to_dict().get(before)
+        return rooms_data
 
 
-def get_reservations():
+def get_reservations(now: datetime) -> List[Optional[Dict]]:
+    """
+    空席通知予約を行ったユーザーと対象学習室名を取得
+    予約から60分以上経った場合はusersコレクションからuser_idに基づくドキュメントを削除する
+
+    Args:
+        now(datetime): 現在時刻
+
+    Returns:
+        list
+    """
     print('### get_users ###')
-    global reservations
-    now = get_time()
-    users_ref = db.collection('users')
-    query_ref = users_ref.where('reserved', '==', True)
-    docs = query_ref.stream()
-    for doc in docs:
-        doc_dic = doc.to_dict()
+    reservations = []
+    users_ref = db.collection('users').stream()
+    for user in users_ref:
+        doc_dic = user.to_dict()
         delta = int((now - doc_dic['reserve_time']).total_seconds() / 60)
-        user_id = doc.id
+        user_id = user.id
         if delta < 60:
             room_name = doc_dic['room_name']
-            reservations.append({
-                'user_id': user_id,
-                'room_name': room_name
-            })
+            reservations.append({'user_id': user_id, 'room_name': room_name})
         else:
-            users_ref.document(user_id).update({'reserved': False})
+            delete_reservation(user_id)
+    return reservations
 
 
-def push_messages(user_id, room_name):
+def push_messages(user_id: str, room_name: str) -> None:
+    """
+    クライアントにメッセージを送信する
+
+    Args:
+        user_id(str): LINEのユーザーID
+        room_name(str): 学習室名
+    """
     message = TextSendMessage(text=f'【{room_name}】空席ができました。')
     line_bot_api.push_message(user_id, message)
-    print(f'* pushed --> {user_id}')
+    print(f'* Message has been pushed -> user_id: {user_id}')
 
 
-def delete_reservation(user_id):
-    user_doc = db.collection('users').document(user_id)
-    user_doc.update({'reserved': False})
-    print(f'* delete reservation -> {user_id}')
+def delete_reservation(user_id: str) -> None:
+    """
+    users内のuser_idに基づくドキュメントを削除する
+
+    Args:
+        user_id: LINEのユーザーID
+    """
+    db.collection('users').document(user_id).delete()
+    print(f'* Deleted the document -> user_id: {user_id}')
 
 
-def delete_all_reservation():
-    print('### delete_all_reservation ###')
+def delete_all_reservations() -> None:
+    """
+    usersコレクション内のドキュメントを全て削除する
+    """
+    print('## delete_all_reservations ##')
     users_ref = db.collection('users')
-    query_ref = users_ref.where('reserved', '==', True).stream()
-    for user in query_ref:
-        users_ref.document(user.id).update({'reserved': False})
+    for user in users_ref.stream():
+        users_ref.document(user.id).delete()
+        print(f'* Deleted the document -> user_id: {user.id}')
 
 
-def check():
+def check(rooms: Dict, reservations: List) -> None:
+    """
+    空席通知予約を行った学習室に空席があったら通知する
+    通知を行った場合通知を行ったユーザーをusersコレクションから削除する
+
+    Args:
+        rooms(dict): 各学習室の空席情報
+        reservations(list)): 空席通知予約を行ったユーザーIDと学習室名
+    """
     print('### check ###')
-    if rooms:
-        for reservation in reservations:
-            for room in rooms['data']:
-                if reservation['room_name'] == room['name']:
-                    if room['seats_num'] > 0:
-                        push_messages(reservation['user_id'], reservation['room_name'])
-                        delete_reservation(reservation['user_id'])
-    else:
-        delete_all_reservation()
+    for reservation in reservations:
+        for room in rooms['data']:
+            if reservation['room_name'] == room['name']:
+                if room['seats_num'] > 0:
+                    push_messages(reservation['user_id'], reservation['room_name'])
+                    delete_reservation(reservation['user_id'])
 
 
 def run(Request):
-    print('run...')
 
-    # グローバル変数の初期化
-    init_var()
+    now = datetime.now(jst)
+
+    print('run...')
+    print(now)
 
     # 学習室の座席情報の取得
-    get_rooms()
+    rooms = get_rooms_data(now)
 
-    # 通知を行うユーザーの抽出
-    get_reservations()
-
-    # 予約ごとに空席を確認しメッセージ送信
-    check()
+    if rooms:
+        # 空席通知予約を行ったユーザーと学習室名の取得
+        reservations = get_reservations(now)
+        # 予約ごとに空席を確認しメッセージ送信
+        check(rooms, reservations)
+    else:
+        # 空席情報が取得できなかった場合（開館時間外）usersコレクション内の全てのドキュメントを削除する
+        delete_all_reservations()
 
     return 'ok'
 
